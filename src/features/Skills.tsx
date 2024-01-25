2a23e24bb09d26751e1e8fdeb80ec9b730cc4c9a
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2024-01-25 20:38:24 +0200

    testing img 2

--- src/features/Skills.tsx
+++ src/features/Skills.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import MyBox from "../app/components/MyBox";
+import CSharpLogo from "/public/Images/icons8-c-sharp-logo-50.png";
 
 export default function Skills() {
   return (
@@ -11,6 +12,7 @@ export default function Skills() {
           alignItems: "center",
         }}
       >
+        <img src={CSharpLogo} alt="logo" />
         1
         <img
           src="./portfolio/Images/icons8-c-sharp-logo-50.png"
@@ -24,19 +26,9 @@ export default function Skills() {
           src="./portfolio/public/Images/icons8-c-sharp-logo-50.png"
           alt="C-Sharp"
         />
-        6
-        <img
-          src="../portfolio/Images/icons8-c-sharp-logo-50.png"
-          alt="C-Sharp"
-        />
-        7<img src="public/Images/icons8-c-sharp-logo-50.png" alt="C-Sharp" />
-        8<img src="../Images/icons8-c-sharp-logo-50.png" alt="C-Sharp" />
-        9<img src="../public/Images/icons8-c-sharp-logo-50.png" alt="C-Sharp" />
-        10
-        <img
-          src="../portfolio/public/Images/icons8-c-sharp-logo-50.png"
-          alt="C-Sharp"
-        />
+        <img src={CSharpLogo} alt="logo" />
+        <img src={CSharpLogo} alt="logo" />
+        <img src={CSharpLogo} alt="logo" />
         <Typography color={"textPrimary"}>Started with C#</Typography>
       </Box>
     </MyBox>
