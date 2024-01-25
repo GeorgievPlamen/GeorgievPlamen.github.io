c2a7032377af468e85d6216d87a09820a769680a
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2024-01-25 20:31:44 +0200

    images test

--- src/features/Skills.tsx
+++ src/features/Skills.tsx
@@ -11,12 +11,30 @@ export default function Skills() {
           alignItems: "center",
         }}
       >
+        1
         <img
           src="./portfolio/Images/icons8-c-sharp-logo-50.png"
           alt="C-Sharp"
         />
+        2<img src="public/Images/icons8-c-sharp-logo-50.png" alt="C-Sharp" />
+        3<img src="./Images/icons8-c-sharp-logo-50.png" alt="C-Sharp" />
+        4<img src="./public/Images/icons8-c-sharp-logo-50.png" alt="C-Sharp" />
+        5
         <img
           src="./portfolio/public/Images/icons8-c-sharp-logo-50.png"
+          alt="C-Sharp"
+        />
+        6
+        <img
+          src="../portfolio/Images/icons8-c-sharp-logo-50.png"
+          alt="C-Sharp"
+        />
+        7<img src="public/Images/icons8-c-sharp-logo-50.png" alt="C-Sharp" />
+        8<img src="../Images/icons8-c-sharp-logo-50.png" alt="C-Sharp" />
+        9<img src="../public/Images/icons8-c-sharp-logo-50.png" alt="C-Sharp" />
+        10
+        <img
+          src="../portfolio/public/Images/icons8-c-sharp-logo-50.png"
           alt="C-Sharp"
         />
         <Typography color={"textPrimary"}>Started with C#</Typography>
